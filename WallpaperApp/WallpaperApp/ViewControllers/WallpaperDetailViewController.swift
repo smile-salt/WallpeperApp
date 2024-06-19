0f303dae3960ffb6b8cdea6064e25c6d618b58bf
ef(Yosuke_Nishimura) <ef@saltworks.jp>
2024-06-19 11:33:12 +0900

    タップによる背景色変化、他要素の削除まで実装

--- WallpaperApp/WallpaperApp/ViewControllers/WallpaperDetailViewController.swift
+++ WallpaperApp/WallpaperApp/ViewControllers/WallpaperDetailViewController.swift
@@ -17,6 +17,11 @@ class WallpaperDetailViewController: UIViewController {
     @IBOutlet private var locationLabel: UILabel!
     @IBOutlet private var updateAtLabel: UILabel!
     
+    @IBOutlet weak var detailInfomationStackView: UIStackView!
+    @IBOutlet weak var photoInfoLabel: UILabel!
+    
+    var isPhotoViewing = false
+    
     init(wallpaper: UnsplashPhoto) {
         self.wallpaper = wallpaper
         super.init(nibName: nil, bundle: nil)
@@ -30,7 +35,15 @@ class WallpaperDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        
         imageView.image = photoImage
+        imageView.isUserInteractionEnabled = true
+        let photoTapGesture = UITapGestureRecognizer(target: self, action: #selector(tappedPhotoImage))
+        imageView.addGestureRecognizer(photoTapGesture)
+        
+        view.isUserInteractionEnabled = true
+        let viewTapGesture = UITapGestureRecognizer(target: self, action: #selector(tappedView))
+        view.addGestureRecognizer(viewTapGesture)
         
         guard let wallpaper = wallpaper else {
             return
@@ -59,6 +72,41 @@ class WallpaperDetailViewController: UIViewController {
         }
         UIApplication.shared.open(url)
     }
+    
+    @objc func tappedPhotoImage() {
+        togglePhotoViewingStyle()
+    }
+    
+    @objc func tappedView() {
+        // 詳細ビュー状態の解除は写真以外の箇所でも反応する必要があるため実装
+        if isPhotoViewing {
+            togglePhotoViewingStyle()
+        }
+    }
+    
+    
+    func togglePhotoViewingStyle() {
+        if isPhotoViewing {
+            detailInfomationStackView.isHidden = false
+            photoInfoLabel.isHidden = false
+            navigationItem.hidesBackButton = false
+            UIView.animate(withDuration: 0.2) {
+                self.view.backgroundColor = .white
+            }
+        } else {
+            detailInfomationStackView.isHidden = true
+            photoInfoLabel.isHidden = true
+            navigationItem.hidesBackButton = true
+            UIView.animate(withDuration: 0.3) {
+                // 指定のカラーコード
+                self.view.backgroundColor = UIColor(red: 47/255, green: 47/255, blue: 47/255, alpha: 1.0)
+            }
+        }
+        
+        isPhotoViewing = !isPhotoViewing
+    }
+    
+    
     
 }
 
