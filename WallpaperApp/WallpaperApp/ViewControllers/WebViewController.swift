0c27245b22ee3d12558c245aa2a687483aba0946
school06 <>
2024-06-24 09:38:35 +0900

    もう1枚モーダルを出そうと思い、作業途中です（未完成）

---
+++ WallpaperApp/WallpaperApp/ViewControllers/WebViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WebViewController.swift
+//  WallpaperApp
+//
+//  Created by school06 on 2024/06/21.
+//
+
+import UIKit
+
+class WebViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
